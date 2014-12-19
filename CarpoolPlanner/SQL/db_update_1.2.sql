e2e115bbef793ca677132b1ba9a0f79387c8eb40
AJ Richardson <aj_r@shaw.ca>
2014-12-18 23:16:23 -0500

    Modified DB scripts to work for 2 different databases

--- CarpoolPlanner/SQL/db_update_1.2.sql
+++ CarpoolPlanner/SQL/db_update_1.2.sql
@@ -1,21 +1,21 @@
-ALTER TABLE `climbing`.`user_trip_instance` 
+ALTER TABLE `user_trip_instance` 
 CHANGE COLUMN `attending` `attending` BIT(1) NULL DEFAULT NULL,
 ADD COLUMN `confirm_time` DATETIME NULL AFTER `attending`,
 ADD COLUMN `no_room` BIT(1) NOT NULL DEFAULT b'0' AFTER `trip_id`;
 
-ALTER TABLE `climbing`.`user_trip_instance` 
+ALTER TABLE `user_trip_instance` 
 DROP FOREIGN KEY `fk_usertripinstance_usertrip`;
-ALTER TABLE `climbing`.`user_trip_instance` 
+ALTER TABLE `user_trip_instance` 
 ADD CONSTRAINT `fk_usertripinstance_usertrip`
   FOREIGN KEY (`user_id` , `trip_id`)
-  REFERENCES `climbing`.`user_trip` (`user_id` , `trip_ID`)
+  REFERENCES `user_trip` (`user_id` , `trip_ID`)
   ON DELETE CASCADE
   ON UPDATE CASCADE;
   
-UPDATE `climbing`.`user_trip_instance`
+UPDATE `user_trip_instance`
 SET `attending` = NULL
 WHERE `attending` = b'1';
 
-ALTER TABLE `climbing`.`trip_instance` 
+ALTER TABLE `trip_instance` 
 ADD COLUMN `drivers_picked` BIT(1) NOT NULL DEFAULT b'0' AFTER `skip`;
 
