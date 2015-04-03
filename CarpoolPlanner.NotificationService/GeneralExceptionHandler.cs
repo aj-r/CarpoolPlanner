5c5adf25653c91f33f540c3a4c09496590ec176c
AJ Richardson <aj.ric.ar+git@gmail.com>
2015-04-02 20:50:56 -0400

    Added missing files

---
+++ CarpoolPlanner.NotificationService/GeneralExceptionHandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarpoolPlanner.NotificationService
+{
+    public static class GeneralExceptionHandler
+    {
+        public static event Action<Exception> ExceptionEncountered;
+
+        /// <summary>
+        /// Raises the ExceptionEncountered event.
+        /// </summary>
+        /// <param name="ex">The exception that was thrown.</param>
+        public static void Raise(Exception ex)
+        {
+            if (ExceptionEncountered != null)
+                ExceptionEncountered(ex);
+        }
+
+        /// <summary>
+        /// Raises the ExceptionEncountered event.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public static void Raise(object sender, UnhandledExceptionEventArgs e)
+        {
+            Raise(e.ExceptionObject as Exception);
+        }
+    }
+}
---
+++ CarpoolPlanner.NotificationService/TaskExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+
+namespace CarpoolPlanner.NotificationService
+{
+    /// <summary>
+    /// Contains extensions for Tasks.
+    /// </summary>
+    public static class TaskExtensions
+    {
+        /// <summary>
+        /// Executes a task and sends any exceptions to the general exception handler.
+        /// </summary>
+        /// <param name="task"></param>
+        public static void WithErrorHandler(this Task task)
+        {
+            task.ContinueWith(t => GeneralExceptionHandler.Raise(t.Exception), TaskContinuationOptions.OnlyOnFaulted);
+        }
+    }
+}
