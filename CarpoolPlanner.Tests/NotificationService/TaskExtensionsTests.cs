32a0c11f815aa1944215ebb8b2cc5c196852800e
AJ Richardson <aj.ric.ar+git@gmail.com>
2015-04-02 21:02:18 -0400

    Added another missing file

---
+++ CarpoolPlanner.Tests/NotificationService/TaskExtensionsTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using CarpoolPlanner.NotificationService;
+using NUnit.Framework;
+
+namespace CarpoolPlanner.Tests.NotificationService
+{
+    [TestFixture]
+    public class TaskExtensionsTests
+    {
+        [Test]
+        public async Task TaskWithErrorHandler_ShouldCallHandler()
+        {
+            var exceptionCount = 0;
+            GeneralExceptionHandler.ExceptionEncountered += ex => ++exceptionCount;
+            var tcs = new TaskCompletionSource<bool>();
+            tcs.Task.WithErrorHandler();
+
+            tcs.SetException(new Exception());
+
+            await Task.Yield();
+
+            Assert.That(exceptionCount, Is.EqualTo(1));
+        }
+    }
+}
